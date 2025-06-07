0fcba3507748f5205bbfba93c25544d70c517b4c
Belmejdoub Mohammed <mohamed.belmejdoub@saphirSystems.com>
2025-06-07 12:24:10 +0100

    save

---
+++ src/main/java/com/chronoforce/project/WorkTrackApplication.java
@@ -0,0 +1,13 @@
+package com.chronoforce.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkTrackApplication.class, args);
+	}
+
+}
